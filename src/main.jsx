7588395aea2db5e4673dea78e598633525aee847
kzmztrk <kazimozturk14@gmail.com>
2025-05-01 21:04:02 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import YatirimPaneli from "./YatirimPaneli";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <YatirimPaneli />
+  </React.StrictMode>
+);
